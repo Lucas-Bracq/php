937ae086391f7615f6f616e43230f146edd7fa87
Lucas-Bracq <49907533+Lucas-Bracq@users.noreply.github.com>
2019-09-16 15:57:19 +0200

    Add files via upload

--- TD1/TD1.php
+++ TD1/TD1.php
@@ -1,1 +1,15 @@
 <?php
+function start_page($title){echo'<!DOCTYPE html><html lang="fr"><head><title>'.PHP_EOL.$title.'</title></head><body>'.PHP_EOL;};?>
+<?phpstart_page('GLHF');?>
+<hr/><br/><strong>Test</strong><br/><hr/>
+<?php
+$jour=date('d F, Y');
+$jour=date('d/m/Y', strtotime('2001-03-12'));
+echo $jour;
+echo '<br/>';
+$jour=date('d F, Y,');
+echo $jour,' 10:16 pm';
+?>
+<?phpend_page('GLHF');?>
+
+
