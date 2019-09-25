7725719353e69badfa660491a389c45ade80fbe6
BRACQ Lucas <b18004784@A-F002118.salsa.univ-amu.fr>
2019-09-25 09:16:42 +0200

    Début

---
+++ TD2/index.php
@@ -0,0 +1,8 @@
+<?php
+    include'utils.inc.php';
+    //Correspond à l’adresse IP de celui qui visualise la page.
+    echo getenv('REMOTE_ADDR');
+    //Correspond à l’adresse IP du serveur.
+    echo getenv('HTTP_HOST');
+    //Correspond au logiciel serveur Web utilisé
+    echo getenv('SERVER_SOFTWARE');
---
+++ TD2/utils.inc.php
@@ -0,0 +1,7 @@
+<?php
+    function start_page($title)
+        {
+            echo '<!DOCTYPE html><html
+            lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
+        };
+    start_page(Titre);
