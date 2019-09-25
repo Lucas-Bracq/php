c9e7a7c2baa0df8fec4c8ea1832b2232bc2260e4
Lucas-Bracq <lucas.bracq@etu.univ-amu.fr>
2019-09-25 12:01:29 +0200

    POST

--- TD2/data-processing.php
+++ TD2/data-processing.php
@@ -4,6 +4,17 @@
         $message='Voicivosidentifiants d\'inscription:'.PHP_EOL;
         $message.='Email:'.$_POST['mail'].PHP_EOL;
         $message.='Motdepasse:'.PHP_EOL.$_POST['Vérification'];
+        mail('chiquinha260@gmail.com');
+        echo '<!DOCTYPE html> 
+              <html lang="fr">
+              <head>
+              <title>Message envoyé</title>
+              </head>
+              <body>
+              <header> Merci pour le soutien</header>
+              <ul>
+              <li><a href="index.html">Menu principal</a></li>
+              </ul></body>' .PHP_EOL;
     }
     else{
         echo '<br/><strong> Bouton non géré!</strong>';
