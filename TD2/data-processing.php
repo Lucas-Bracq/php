95f0e93da1720754f2c98880ff581317e8e3fe14
Lucas-Bracq <lucas.bracq@etu.univ-amu.fr>
2019-09-25 11:50:39 +0200

    POST

--- TD2/data-processing.php
+++ TD2/data-processing.php
@@ -1,7 +1,9 @@
 <?php
     include 'formulaire.html';
     if($_POST['Submit'] =='Send'){
-
+        $message='Voicivosidentifiants d\'inscription:'.PHP_EOL;
+        $message.='Email:'.$_POST['mail'].PHP_EOL;
+        $message.='Motdepasse:'.PHP_EOL.$_POST['Vérification'];
     }
     else{
         echo '<br/><strong> Bouton non géré!</strong>';
